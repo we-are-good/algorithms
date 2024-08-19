d1340ea48a3e46bc6ba0157f83aa20db7bea14d1
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-08-19 20:21:16 +0900

    [level 1] Title: 달리기 경주, Time: 318.58 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,19 @@
+function solution(players, callings) {
+    const hash = new Map();
+    
+    players.forEach((name, index) => {
+        hash.set(name, index);
+    })
+
+    callings.forEach(name => {
+        const currIdx = hash.get(name);
+        const front = players[currIdx - 1];
+
+        [players[currIdx], players[currIdx -1]] = [players[currIdx -1], players[currIdx]];
+        
+        hash.set(name, hash.get(name) - 1);
+        hash.set(front, hash.get(name) + 1);
+    })
+    
+    return players;
+}
